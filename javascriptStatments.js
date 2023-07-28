4b71bd62c0cc72439e3f45252d6dcc8891111caa
llcain <llcain2012@gmail.com>
2023-07-27 21:31:37 -0500

    Introduced conditional statements

--- javascriptStatments.js
+++ javascriptStatments.js
@@ -5,4 +5,20 @@
 function myFunction() {
     document.getElementById('demo1').innerHTML = "Susan";
     document.getElementById("demo2").innerHTML = "Willard";
+    document.getElementById('demo').innerHTML = z;
 }
+
+// Assigns value of 5 to x
+let x = 5;
+y = 6;
+z = x + y;
+
+// conditional statement (if statements)
+
+let tempurature = 19;
+
+if (tempurature > 5) {
+    // block of code to be execute if statment is true
+    document.body.style.backgroundColor = "purple";
+}
+
