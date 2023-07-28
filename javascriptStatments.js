3f9e39b6daef7129bd064e9ba0a762a2b45a26ea
llcain <llcain2012@gmail.com>
2023-07-27 20:12:58 -0500

    Added on click function to button

---
+++ javascriptStatments.js
@@ -0,0 +1,8 @@
+// Statments to put onclick on button so text will appear in html
+
+// 1. put onclick on button 2. create function and give function a name 3. grab element by id and put what the text is equal to
+
+function myFunction() {
+    document.getElementById('demo1').innerHTML = "Susan";
+    document.getElementById("demo2").innerHTML = "Willard";
+}
