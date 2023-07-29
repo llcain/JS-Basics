226918317d210f6e3a046611a078f554105298a4
llcain <llcain2012@gmail.com>
2023-07-29 12:40:59 -0500

    console log JSON object

--- jsonplaceholder.js
+++ jsonplaceholder.js
@@ -1,3 +1,14 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+// how to fetch data from database using fetch
+
+/*
+1. fetch from json file
+2. console log the data from JSON file 
+ */
+ 
+fetch('./data/employees.json')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then((json) => console.log(json));
