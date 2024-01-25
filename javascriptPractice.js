6d05be6a4820097cb26631f6cb3c2e3bf84b5dd5
llcain <llcain2012@gmail.com>
2024-01-25 16:51:48 -0600

    added json file

---
+++ javascriptPractice.js
@@ -0,0 +1,17 @@
+let firstName = "Bob";
+let lastName = "Billy";
+let number = 6;
+
+if (number > 10 || number < 2) {
+    document.body.style.backgroundColor = "green";
+}
+
+
+let person = {
+    name: "Mosh",
+    age: 40,
+    email: "codewithmosh@gmail.com"
+};
+console.log(firstName + " " + lastName);
+
+document.getElementById('demo').innerHTML = person.age;
