94b7e1a6e9be3be7a9a0c83fe4fb070e6c5465bc
llcain <llcain2012@gmail.com>
2024-04-27 23:26:09 -0500

    Click button to display date and time

--- myscript.js
+++ myscript.js
@@ -1,25 +1,7 @@
-var button = document.querySelector('button');
-var box = document.getElementById('changeMe');
-
-changeColor = () => {
-    if(box.style.background == 'red') {
-        box.style.background = 'blue';
-    } else {
-        box.style.background = 'red';
-    }   
-}
-
-// Loop to Next on Click
-var box1 = document.getElementById('colors');
-// create array of colors
-const myColors = ["yellow", "green", "orange", "purple", "aqua", "plum"];
 
 
 
-changeMyColors = () => {
-    // document.getElementById('colors').style.background = colors.map(colors[i]);
 
-    color = myColors.shift();
-    myColors.push(color);
-    box1.style.backgroundColor = color;
+clickMe = () => {
+    document.getElementById('demo').innerHTML = Date();
 }
